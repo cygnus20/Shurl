591d2dc010af0dac97ffa302473693134a5b1f34
Mustapha Ojo <ojomustapha@gmail.com>
2024-11-29 23:46:40 +0100

    Add ShurlDbContext

---
+++ Shurl/Data/ShurlDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shurl.Models;
+
+namespace Shurl.Data
+{
+    public class ShurlDbContext(DbContextOptions<ShurlDbContext> options) : DbContext(options)
+    {
+        public DbSet<Urls> Urls { get; set; }
+    }
+}
