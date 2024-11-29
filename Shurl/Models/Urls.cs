a233d3707a793c8faf7147cefc92621e25be5e8b
Mustapha Ojo <ojomustapha@gmail.com>
2024-11-29 23:45:56 +0100

    Add Urls.cs to Models

---
+++ Shurl/Models/Urls.cs
@@ -0,0 +1,9 @@
+﻿namespace Shurl.Models
+{
+    public record Urls
+    {
+        int Id { get; set; }
+        public string LongUrl { get; set; } = "";
+        public string ShortUrl { get; set; } = "";
+    }
+}
