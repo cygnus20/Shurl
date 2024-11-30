79fcaa09ca30779974e800f6e924ec7108af1665
Mustapha Ojo <ojomustapha@gmail.com>
2024-11-30 22:08:08 +0100

    Add UrlService.cs for shortening url

---
+++ Shurl/Core/UrlService.cs
@@ -0,0 +1,17 @@
+﻿namespace Shurl.Core;
+
+public class UrlService
+{
+    private readonly HashService _encoderService;
+    public UrlService(HashService encoderService)
+    {
+        this._encoderService = encoderService;
+    }
+    public string Shorten(string url)
+    {
+        IEncoder encodeStrat = new Base58Encoder();
+        EncoderContext encoderctx = new EncoderContext(encodeStrat);
+        string urlHash = _encoderService.ComputeSha256(url);
+        return encoderctx.BaseEncode(urlHash);
+    }
+}
