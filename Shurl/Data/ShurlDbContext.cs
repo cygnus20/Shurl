86c39a657ed893752cfd0cf5a2232276514edf66
Mustapha Ojo <ojomustapha@gmail.com>
2024-12-02 21:22:43 +0100

    Add query filter to Urls table and create index for the short url key

--- Shurl/Data/ShurlDbContext.cs
+++ Shurl/Data/ShurlDbContext.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shurl.Core;
 using Shurl.Models;
 
 namespace Shurl.Data;
 
-public class ShurlDbContext(DbContextOptions<ShurlDbContext> options) : IdentityDbContext<IdentityUser>(options)
+public class ShurlDbContext(DbContextOptions<ShurlDbContext> options, IGetUserClaims claims) : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<Urls> Urls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        _ = builder.Entity<Urls>()
+            .HasQueryFilter(u => u.UserId == claims.UserId)
+            .HasIndex(u => u.ShortUrl)
+            .IsUnique();
+    }
 }
